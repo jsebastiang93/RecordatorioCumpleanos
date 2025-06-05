0b4f32de3370cb7c0e7c482a3c676b5782a04488
Juan Sebastian Gutierrez <juan.gutierrez02@unicatolica.edu.co>
2025-06-04 20:48:58 -0500

    feat(Backend js view list reminders): :hammer: Implement backend structure to list reminders

---
+++ Backend/srv/controllers/reminderController.js
@@ -0,0 +1,20 @@
+// Ejemplo de función para agregar un nuevo recordatorio
+exports.newReminder = (req, res) => {
+    // Implementación aquí...
+    res.json({ mensaje: 'Funcionalidad en desarrollo' });
+};
+
+exports.editReminder = (req, res) => {
+    // Implementación aquí...
+    res.json({ mensaje: 'Funcionalidad en desarrollo' });
+};
+
+exports.deleteReminder = (req, res) => {
+    // Implementación aquí...
+    res.json({ mensaje: 'Funcionalidad en desarrollo' });
+};
+
+exports.listReminder = (req, res) => {
+    // Implementación aquí...
+    res.json({ mensaje: 'Funcionalidad en desarrollo' });
+};
